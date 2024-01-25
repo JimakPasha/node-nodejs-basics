e4a081f1d62dc41613e2ffb2ae9bfa98fba6eb65
JimakPasha <jimak-pg@yandex.by>
2024-01-25 22:15:27 +0300

    feat: add copy-fs, create-fs fns

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises as fs } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = new URL('files/fresh.txt', import.meta.url);
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+      } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.writeFile(filePath, 'I am fresh and young');
+            console.log('File created successfully');
+        } else {
+            throw err;
+        }
+      }
 };
 
 await create();
