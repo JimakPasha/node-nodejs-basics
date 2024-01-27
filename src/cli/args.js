166409bd09d4f8c41899e1baf3bc51e26d16bf06
JimakPasha <jimak-pg@yandex.by>
2024-01-27 13:43:03 +0300

    feat: add cli fns

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const formattedArgs = [];
+
+    for (let i = 0; i < args.length; i+=2) {
+        formattedArgs.push(`${args[i].slice(2)} is ${args[i+1]}`);
+    }
+    
+    console.log(formattedArgs.join(', '));
 };
 
 parseArgs();
