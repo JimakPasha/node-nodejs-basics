bc58f00a40e4a85971552f7bc1a89b32deae8a93
JimakPasha <jimak-pg@yandex.by>
2024-01-28 18:32:52 +0300

    feat: add streams fns

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = `${__dirname}/files/fileToRead.txt`;
+
+    const input = fs.createReadStream(filePath);
+
+    input.on('data', (chunk) => {
+        process.stdout.write(chunk + '\n');
+    });
+
+    input.on('error', (error) => {
+        throw Error(error);
+    });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const readable = process.stdin;
+    const writable = process.stdout;
+
+    const reverseTransform = new Transform({
+        transform(chunk, _, callback) {
+            const transformedChunk = chunk.toString().trim().split('').reverse().join('');
+
+            this.push(transformedChunk + '\n');
+
+            callback();
+        }
+    });
+
+    pipeline(readable, reverseTransform, writable, (error) => {
+        console.log(`Error: ${error}`);
+    })
 };
 
 await transform();
