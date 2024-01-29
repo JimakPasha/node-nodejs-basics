3b8d79a3e68cf57aac6d5c9634e7498d7805dec4
JimakPasha <jimak-pg@yandex.by>
2024-01-29 22:54:28 +0300

    refactor: fs create fn

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,18 +1,12 @@
-import { promises as fs } from 'fs';
+import { writeFile } from 'fs/promises';
 
 const create = async () => {
     const filePath = new URL('files/fresh.txt', import.meta.url);
 
     try {
-        await fs.access(filePath);
-        throw new Error('FS operation failed');
+        await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
       } catch (err) {
-        if (err.code === 'ENOENT') {
-            await fs.writeFile(filePath, 'I am fresh and young');
-            console.log('File created successfully');
-        } else {
-            throw err;
-        }
+        throw new Error('FS operation failed');
       }
 };
 
